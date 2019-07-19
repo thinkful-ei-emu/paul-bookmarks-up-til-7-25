28c88ba07759bfd5b31375f5087e4d1484ded066
Paul Han <kyutae.han93@gmail.com>
2019-07-19 12:48:53 -0400

    finished for now

---
+++ src/store.js
@@ -0,0 +1,6 @@
+let bookmarks=[
+  {id:'1', title:'Tom Sawyer', content:'Not sure what kind of bookmarks I\'m supposed to be making' },
+  {id:'2', title:'YOLO', content:'Story of all lives except those in the process of reincarnation'}
+];
+
+module.exports=bookmarks;
